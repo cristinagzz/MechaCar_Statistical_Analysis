5063883821e7e01f5cee7b250b9349ca5aff12da
Patricio Brito <patricio@drinkfitzer.com>
2022-10-10 17:56:43 -0500

    derivable 2 challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,6 @@ library(dplyr)
 MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import used Mechacar datase
 lm(mpg ~ vehicle_length + vehicle_weight+ spoiler_angle + ground_clearance + AWD, data=MechaCar) #create linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight+ spoiler_angle + ground_clearance + AWD, data=MechaCar)) #summarize linear model
+Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import used Mechacar dataset
+total_summary <- Suspension_Coil%>%summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), SD=sd(PSI))
+lot_summary <- Suspension_Coil%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), SD=sd(PSI))
